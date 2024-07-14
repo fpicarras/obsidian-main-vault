108e8fdf43daf7b054fe0988088b0d925253b80f
Filipe Piçarra <fncp.1703@gmail.com>
2024-07-14 21:23:29 +0100

    vault backup: 2024-07-14 21:23:29

---
+++ GoldenRift/villagers.py
@@ -0,0 +1,8 @@
+class Villager:
+    def __init__(self, name: str, type: str, profession: str, level: int) -> None:
+        self.name = name
+        self.type = type
+        self.prof = profession
+        self.level = level
+
+    
