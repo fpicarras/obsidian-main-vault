e15ca5a39dffcbbe8a2e5903f75ed548e4755cc3
Filipe Piçarra <fncp.1703@gmail.com>
2024-07-14 21:33:29 +0100

    vault backup: 2024-07-14 21:33:29

--- GoldenRift/villagers.py
+++ GoldenRift/villagers.py
@@ -4,5 +4,31 @@ class Villager:
         self.type = type
         self.prof = profession
         self.level = level
+        self.trades = []
+
+    def addTrade(self, buyId: str, b_count: int, sellId: str, s_count: int, maxUses = 9999999):
+        aux = dict(buy = dict(id = buyId, Count = b_count), sell = dict(id = sellId, Count = s_count), maxUses = maxUses)
+        self.trades.append(aux)
+
+    def format_dict(self, d):
+        if isinstance(d, dict):
+            formatted_items = []
+            for key, value in d.items():
+                formatted_items.append(f"{key}: {self.format_dict(value)}")
+            return "{" + ", ".join(formatted_items) + "}"
+        elif isinstance(d, list):
+            formatted_items = [self.format_dict(item) for item in d]
+            return "[" + ", ".join(formatted_items) + "]"
+        elif isinstance(d, str):
+            return f"'{d}'"
+        else:
+            return str(d)
+
+    def getCommand(self):
+        return self.format_dict(dict(VillagerData = dict(profession=self.prof, level=self.level, type=self.type), Offers = dict(Recipes = self.trades)))
+
+v = Villager("test", "plains", "farmer", "5")
+v.addTrade("emerald", )
+
 
     
